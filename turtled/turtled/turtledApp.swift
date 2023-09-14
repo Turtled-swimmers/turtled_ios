464822f9c78f5edc9355582366ca39bc76eed5d7
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-09-14 20:23:45 +0900

    feat : ios 초기셋팅

---
+++ turtled/turtled/turtledApp.swift
@@ -0,0 +1,17 @@
+//
+//  turtledApp.swift
+//  turtled
+//
+//  Created by 서희찬 on 2023/09/14.
+//
+
+import SwiftUI
+
+@main
+struct turtledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
