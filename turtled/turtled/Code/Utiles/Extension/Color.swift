c9a0be4b66760303b4abb37dcf70a29a013db98d
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-10-20 04:09:44 +0900

    feat : color setting

--- turtled/turtled/Code/Utiles/Extension/Color.swift
+++ turtled/turtled/Code/Utiles/Extension/Color.swift
@@ -5,5 +5,11 @@ import SwiftUI
 extension Color {
     static let MainColor = Color("main")
     static let Gray6 = Color("gray6")
+    static let Gray5 = Color("gray5")
+    static let Gray4 = Color("gray4")
+    static let Gray3 = Color("gray3")
+    static let Gray2 = Color("gray2")
+    static let Gray1 = Color("gray1")
     static let Black = Color("black")
+    static let White = Color("white")
 }
